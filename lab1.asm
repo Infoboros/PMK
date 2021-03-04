4b4b30f7d90245021f48dc9013ec6963214b2ecc
[       ] <pintastd@gmail.com>
2021-03-04 23:02:22 +0300

    lab1 add 7ind

--- lab1.asm
+++ lab1.asm
@@ -19,7 +19,7 @@ main:
 	; Настройка таймера
 	; При переполнении записывается из TL1 в TH1 (M1 = 1, M0 = 0)
 	MOV TMOD, #20H 
-	MOV TH1, #243 
+	MOV TH1, #243
 	MOV TL1, #243 
 	SETB TR1 ; запуск таймера 1
 	; Настройка прерывания
@@ -27,10 +27,23 @@ main:
 	SETB EX1 ; прерывание от INT1
 	SETB EA	; разрешить все прерывания
 	
+	mov r1, #0FFh
+	mov r2, #0FFh
+	mov r3, #0FFh
+	mov r4, #0FFh
+	
+; Бесконечный цикл вывода в семи сегментный индикатор
+mainLoop:
 ; Устанавливаем в порте 0, чтобы ждать прерывания при нажатии
 	mov p0, #0
-; Бесконечный пустой цикл
-	sjmp $
+	mov b,#10
+	djnz b, $
+	CLR EX1
+	
+	call printToSeg
+	
+	SETB EX1
+	jmp mainLoop
 	
 ; Сканирование по рядам
 scanKeyBoard:
@@ -56,12 +69,15 @@ scanKeyBoard:
 ; Сканирование первого ряда
 scanRow1:
 	MOV A, #31h
+	MOV r0, #11111001b
  	JNB P0.6, pressKeyR1
 	
 	MOV A, #32h
+	MOV r0, #10100100b
  	JNB P0.5, pressKeyR1
 	
 	MOV A, #33h
+	MOV r0, #10110000b
 	JNB P0.4, pressKeyR1
  	
 	ret ; ключ не найден
@@ -73,12 +89,15 @@ scanRow1:
 ; Сканирование второго ряда
 scanRow2:
 	MOV A, #34h
+	MOV r0, #10011001b
  	JNB P0.6, pressKeyR2
 	
 	MOV A, #35h
+	MOV r0, #10010010b
  	JNB P0.5, pressKeyR2
 	
 	MOV A, #36h
+	MOV r0, #10000010b
 	JNB P0.4, pressKeyR2
  	
 	ret ; ключ не найден
@@ -90,12 +109,15 @@ scanRow2:
 ; Сканирование третьего ряда
 scanRow3:
 	MOV A, #37h
+	MOV r0, #11111000b
  	JNB P0.6, pressKeyR3
 	
 	MOV A, #38h
+	MOV r0, #10000000b
  	JNB P0.5, pressKeyR3
 	
 	MOV A, #39h
+	MOV r0, #10010000b
 	JNB P0.4, pressKeyR3
  	
 	ret ; ключ не найден
@@ -104,15 +126,18 @@ scanRow3:
 	call putc
 	ret
 
-; Сканирование третьего ряда
+; Сканирование четвертого ряда
 scanRow4:
 	MOV A, #02ah
+	MOV r0, #01111111b
  	JNB P0.6, pressKeyR4
 	
 	MOV A, #30h
+	MOV r0, #11000000b
  	JNB P0.5, pressKeyR4
 	
 	MOV A, #23h
+	MOV r0, #10111111b
 	JNB P0.4, pressKeyR4
  	
 	ret ; ключ не найден
@@ -121,11 +146,72 @@ scanRow4:
 	call putc
 	ret
 	
-;печать символа из А на UART
+;печать символа из А на UART и запись символа в буфер семисегментного индикатора
 putc:
 	MOV C, P
 	MOV ACC.7, C ; бит чётности
 	MOV SBUF, A 	;отправляем символ на печать
 	JNB TI, $	;ожидаем пока выведется
 	CLR TI		;очищаем флаг на будущее
+	call shift
+	ret
+	
+;вывод символов из регистров r1-r4 на семисегментный индикатор
+printToSeg:
+	
+	SETB p0.7
+	;сегмент 1
+	clr p3.3
+	clr p3.4
+	mov p1, r1
+	;для нормального отображения
+	call pause
+	mov p1, #0FFh
+
+ 	;сегмент 2
+	setb p3.3
+	clr p3.4
+	mov p1, r2
+	call pause
+	mov p1, #0FFh
+
+ 	;сегмент 3
+	clr p3.3
+	setb p3.4
+	mov p1, r3
+	call pause
+	mov p1, #0FFh
+	
+	;сегмент 4
+	setb p3.3
+	setb p3.4
+	mov p1, r4
+	call pause
+	mov p1, #0FFh	
+	
+	ret
+
+;ожидание что бы цифра проявилась
+pause:
+	mov b,#200
+	djnz b, $
+	ret
+
+;функция делает следующее
+;r4=r3
+;r3=r2
+;r2=r1
+;r1=r0
+shift:
+	mov A, r3
+	mov r4, A
+
+	mov A, r2
+	mov r3, A
+
+	mov A, r1
+	mov r2, A
+
+	mov A, r0
+	mov r1, A
 	ret
